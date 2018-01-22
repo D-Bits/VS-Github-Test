609679617b54c74b50f46b33baabaaba557b3a7a
Dana <dana.lockwood2@seattlecolleges.edu>
2018-01-22 11:26:53 -0800

    Second commit

--- BkReviewGitTest/Controllers/HomeController.cs
+++ BkReviewGitTest/Controllers/HomeController.cs
@@ -3,17 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using BkReviewGitTest.Models;
 
 namespace BkReviewGitTest.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public ActionResult Index() // Controller for the Index page
         {
-            return View();
+            BookReviewDbEntities db = new BookReviewDbEntities();
+            var bks = (from b in db.Books
+                      select b).ToList();
+
+            return View(bks); 
         }
 
-        public ActionResult About()
+        public ActionResult About() 
         {
             ViewBag.Message = "Your application description page.";
 
